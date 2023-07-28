d91ac9a01cdcfdd9db29f6863d15b4b77300bd6b
kochkaev <edukisel@yandex.ru>
2023-07-28 21:59:03 +0700

    Started creating the GetSQL.cs namespace.

---
+++ Assets/GetSQL.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GetSQL
+{
+    public int GetRowFromColumnValue(int column, string value)
+    {
+        
+    }
+    public string GetString(int column, int row)
+    {
+        
+    }
+    public int GetInt(int column, int row)
+    {
+        
+    }
+}
